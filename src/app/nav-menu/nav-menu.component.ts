0e787c93609f6ee0708dee5f7095653fee3cba06
99062735 <99067235@mydavinci.nl>
2023-06-20 11:14:04 +0200

    Made the logo disappear after clicking it.

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -9,30 +9,16 @@ export class NavMenuComponent {
   movableButtonStyles: any = {
     position: 'relative',
     left: '0',
-    transition: 'left 0.01s'
+    transition: 'left'
   };
   clickCounter = 0;
-  gifUrls = ['https://media.tenor.com/O0HXLFkP5VcAAAAi/among-us.gif',
-    'https://media.tenor.com/XTsLyyT2KRgAAAAC/thumbs-up-simon-cowell.gif',
-  'https://media.tenor.com/2kjvgH8AC5EAAAAd/your-mom-doing-your-mom.gif',
-  'https://media.tenor.com/YcXDra0MAnoAAAAM/bobux-0bobux.gif']
   secretEvent() {
     this.clickCounter += 1;
-    const randomGif = Math.floor(Math.random() * this.gifUrls.length);
     if (this.clickCounter === 5) {
-      Swal.fire({
-        title: 'Congratulations!',
-        text: 'You won free robux. Click here to claim your prize:',
-        imageUrl: this.gifUrls[randomGif],
-        showCancelButton: true,
-        confirmButtonText: 'Claim prize',
-        cancelButtonText: 'Ignore',
-        allowOutsideClick: false,
-      }).then((result) => {
-        if (result.isConfirmed) {
-          window.open('https://www.youtube.com/watch?v=xvFZjo5PgG0&pp=ygUOcmlja3JvbGwgbm8gYWQ%3D', '_blank')
-        }
-      })
+      const logoImage = document.getElementById('logo');
+      if (logoImage !== null) {
+        logoImage.id = 'logoImage';
+      }
       this.clickCounter = 0;
     }
   }
