36ea55f0658604f318cf898fe85f78253fa09e26
99062735 <99067235@mydavinci.nl>
2023-06-20 13:59:26 +0200

    Added an effect when the user likes the page, and the login page is added.

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -12,6 +12,10 @@ export class NavMenuComponent {
     transition: 'left'
   };
   clickCounter = 0;
+  gifUrls = ['https://media.tenor.com/O0HXLFkP5VcAAAAi/among-us.gif',
+    'https://media.tenor.com/XTsLyyT2KRgAAAAC/thumbs-up-simon-cowell.gif',
+    'https://media.tenor.com/2kjvgH8AC5EAAAAd/your-mom-doing-your-mom.gif',
+    'https://media.tenor.com/YcXDra0MAnoAAAAM/bobux-0bobux.gif']
   secretEvent() {
     this.clickCounter += 1;
     if (this.clickCounter === 5) {
@@ -21,5 +25,22 @@ export class NavMenuComponent {
       }
       this.clickCounter = 0;
     }
+  }
+
+  likePage() {
+    const randomGif = Math.floor(Math.random() * this.gifUrls.length);
+    Swal.fire({
+      title: 'Congratulations!',
+      text: 'You won free robux. Click here to claim your prize:',
+      imageUrl: this.gifUrls[randomGif],
+      showCancelButton: true,
+      confirmButtonText: 'Claim prize',
+      cancelButtonText: 'Ignore',
+      allowOutsideClick: false,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.open('https://www.youtube.com/watch?v=xvFZjo5PgG0&pp=ygUOcmlja3JvbGwgbm8gYWQ%3D', '_blank')
+      }
+    })
   }
 }
